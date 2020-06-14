37c71c3f3fcc8158efe2ed05fa08c18caa16d6f8
maroua-briki <maroua.briki.ensit@gmail.com>
2020-06-15 00:39:45 +0100

    footer

---
+++ footer-mf/src/app/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'footer-mf-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
